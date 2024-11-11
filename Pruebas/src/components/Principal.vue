affcee90ea360eee2a1289edce77b7e9947a7182
Miguel.Gómez <gomez.mitjavila@gmail.com>
2024-11-11 18:14:02 +0100

    Primeros 3 videos

---
+++ Pruebas/src/components/Principal.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import { defineProps } from 'vue';
+     const props=defineProps({
+        sTitulo:{
+            type:String,
+            default:'Titulo de tu app'
+        }
+    });
+
+
+</script>
+
+<template>
+    <h1> {{sTitulo}} </h1>
+ 
+</template>
+
+<style scoped>
+
+
+</style>
